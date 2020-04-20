a12b50dff66b003bb6b4adfba3b435c7947ebd73
jotaro-sama <36264038+jotaro-sama@users.noreply.github.com>
2020-04-21 00:17:28 +0200

    add custom binary to patch

--- jotaro-sama/print_libs/run.sh
+++ jotaro-sama/print_libs/run.sh
@@ -1,6 +1,6 @@
 #/bin/sh
-gcc -lm hashme.c
-python3 patch_exec.py
-gcc hooks.s hook.c ../AFLplusplus/afl-llvm-rt.o -o hooks.so -fPIC -shared -ldl
+gcc hashme.c -lm
+python3 patch_exec.py a.out
+gcc hooks.s hook.c ../../AFLplusplus/afl-llvm-rt.o -o hooks.so -fPIC -shared -ldl
 env LD_PRELOAD=./hooks.so ./hashme 4
 AFL_PRELOAD=./hooks.so ../../AFLplusplus/afl-showmap -o - -- ./hashme
