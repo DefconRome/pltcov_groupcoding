306f7f86f6814b46d91bacea36da6b1d65d15d49
removedporn <86824510+removedporn@users.noreply.github.com>
2022-01-18 12:55:48 +0800

    Delete andreaf directory

--- andreaf/print_imports.py
+++
@@ -1,9 +0,0 @@
-import lief
-import sys
-
-elf = lief.parse(sys.argv[1])
-
-for idx, sym in enumerate(elf.imported_symbols):
-    if sym.type == lief.ELF.SYMBOL_TYPES.FUNC:
-        print(sym.name)
-
