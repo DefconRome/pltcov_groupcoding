d648e29f5f317d1112526b492eb91188ab2035e6
removedporn <86824510+removedporn@users.noreply.github.com>
2022-01-18 12:56:22 +0800

    Delete borzacchiello directory

--- borzacchiello/stuff/modify_symbol.py
+++
@@ -1,9 +0,0 @@
-import lief
-
-binary  = lief.parse("main")
-for sim in binary.imported_symbols:
-	if sim.type == lief.ELF.SYMBOL_TYPES.FUNC:
-		if sim.name == "puts":
-			sim.name = "system"
-binary.write("main.obj")
-
