a49c3dd239df9a4423cef9f4ab960f3795741cb8
removedporn <86824510+removedporn@users.noreply.github.com>
2022-01-18 12:57:17 +0800

    Delete jotaro-sama/print_libs directory

--- jotaro-sama/print_libs/run.sh
+++
@@ -1,6 +0,0 @@
-#/bin/sh
-gcc hashme.c -lm
-python3 patch_exec.py a.out
-gcc hooks.s hook.c ../../AFLplusplus/afl-llvm-rt.o -o hooks.so -fPIC -shared -ldl
-env LD_PRELOAD=./hooks.so ./hashme 4
-AFL_PRELOAD=./hooks.so ../../AFLplusplus/afl-showmap -o - -- ./hashme
